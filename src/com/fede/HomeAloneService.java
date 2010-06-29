09d6306207ba1253091da90c7c3d44f151212db6
fede <fedepaol@gmail.com>
2010-06-30 00:12:08 +0200

    Moved service to intentservice

--- src/com/fede/HomeAloneService.java
+++ src/com/fede/HomeAloneService.java
@@ -1,11 +1,12 @@
 package com.fede;
 
+import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.IBinder;
 
-public class HomeAloneService extends Service {
+public class HomeAloneService extends IntentService {
 	static final String EVENT_TYPE = "Event type";
 	static final String RECEIVED_CALL = "Received Call";	
 	static final String NUMBER = "Number";
@@ -23,15 +24,7 @@ public class HomeAloneService extends Service {
 	}
 	
 	@Override public int onStartCommand(Intent intent, int flags, int startId) {
-		Bundle extras = intent.getExtras();            
-		String type = extras.getString(EVENT_TYPE);
-		if(type == RECEIVED_SMS){
-			state.handleSms(this, extras);
-		}
 		
-		if(type == RECEIVED_CALL){
-			state.handleIncomingCall(this, extras);
-		}
 						
 		return Service.START_NOT_STICKY;
 	}
@@ -44,10 +37,32 @@ public class HomeAloneService extends Service {
 		return null;
 	}
 	
+	
+	// this is not that useful since this is an use & throw away service
 	public void setState(ServiceState s)
 	{
 		state = s;
 		HomeAloneUtils.setStatus(s.getServiceState(), this);
+	}
+
+	public HomeAloneService(String name)
+	{
+		super(name);
+	}
+	
+	
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		Bundle extras = intent.getExtras();            
+		String type = extras.getString(EVENT_TYPE);
+		if(type == RECEIVED_SMS){
+			state.handleSms(this, extras);
+		}
+		
+		if(type == RECEIVED_CALL){
+			state.handleIncomingCall(this, extras);
+		}
+		
 	}
 
 }
