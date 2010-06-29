4cc230802e9d41a6267358b27229066bbb38b700
fede <fedepaol@gmail.com>
2010-06-30 00:11:48 +0200

    Began trace function from preferences

--- src/com/fede/HomeAloneUtils.java
+++ src/com/fede/HomeAloneUtils.java
@@ -3,6 +3,7 @@ package com.fede;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 
 public class HomeAloneUtils {
@@ -23,5 +24,27 @@ public class HomeAloneUtils {
 		SharedPreferences mySharedPreferences = c.getSharedPreferences(PREF_NAME, mode);
 		SharedPreferences.Editor editor = mySharedPreferences.edit();	
 		editor.putBoolean(STATUS_ENABLED, enabled);
+	}
+	
+	public static String getPreferencesStatus(Context c)
+	{
+		StringBuffer b = new StringBuffer();
+		if(homeAloneEnabled(c)){
+			b.append("s:on ");	
+		}else{
+			b.append("s:off ");
+		}
+		
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(c);
+		
+		String SMS_ENABLE_KEY = c.getString(R.string.forward_to_sms_key);
+		String SMS_TO_FWD_KEY = c.getString(R.string.sms_to_forward_key);
+		
+		if(prefs.getBoolean(SMS_ENABLE_KEY, false) == true){
+			b.append("sms:" + prefs.getString(SMS_TO_FWD_KEY, "") + " ");
+		}
+		
+		
+		return b.toString();
 	}
 }
