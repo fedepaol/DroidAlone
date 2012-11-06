b35b7606b2e4668ce76d9e9d8e29e492a364ad90
fede <fedepaol@gmail.com>
2012-11-06 17:23:46 +0100

    Added Content provider (to be tested yet)

---
+++ src/com/fede/DroidContentProvider.java
@@ -0,0 +1,330 @@
+package com.fede;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fede
+ * Date: 11/6/12
+ * Time: 9:10 AM
+ * To change this template use File | Settings | File Templates.
+ */
+/**
+ * Listing 8-14: A skeleton Content Provider implementation
+ */
+
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.text.TextUtils;
+import android.util.Log;
+
+public class DroidContentProvider extends ContentProvider {
+
+
+    private static final String DATABASE_NAME = "homeAloneDb.db";
+
+    private static final int DATABASE_VERSION = 7;
+
+    //Events
+    private static final String EVENT_TABLE = "Events";
+    public static final String EVENT_DESCRIPTION_KEY = "Desc";
+    public static final int EVENT_DESCRIPTION_COLUMN = 1;
+    public static final String EVENT_TIME_KEY = "Time";
+    public static final int EVENT_TIME_COLUMN = 2;
+    public static final String SHORT_DESC_KEY = "ShortDesc";
+    public static final int SHORT_DESC_COLUMN = 3;
+    public static final String ROW_ID = "_id";
+    private static final int ALLEVENTS = 1;
+    private static final int SINGLE_EVENT = 2;
+    public static final Uri EVENTS_URI = Uri.parse("content://com.fede.dbprovider/events");
+
+
+    // Missed calls
+    private static final String CALL_TABLE = "Calls";
+    public static final String CALL_NUMBER_DESC_KEY = "Number";
+    public static final int CALL_NUMBER_DESC_COLUMN = 1;
+    public static final String CALL_TIME_KEY = "Time";
+    private static final int ALLCALLS = 3;
+    private static final int SINGLE_CALL = 4;
+
+    public static final Uri CALLS_URI = Uri.parse("content://com.fede.dbprovider/calls");
+
+
+
+
+    private static final UriMatcher uriMatcher;
+
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI("com.fede.dbprovider",
+                "events", ALLEVENTS);
+        uriMatcher.addURI("com.fede.dbprovider",
+                "events/#", SINGLE_EVENT);
+        uriMatcher.addURI("com.fede.dbprovider",
+                "calls", ALLCALLS);
+        uriMatcher.addURI("com.fede.dbprovider",
+                "calls/#", SINGLE_CALL);
+    }
+
+
+
+    private static final String DATABASE_EVENT_CREATE = "create table " +
+            EVENT_TABLE + " (" + ROW_ID +
+            " integer primary key autoincrement, " +
+            EVENT_DESCRIPTION_KEY + " string, " +
+            EVENT_TIME_KEY + " integer, " +
+            SHORT_DESC_KEY + " string);";
+
+    private static final String DATABASE_CALLS_CREATE = "create table " +
+            CALL_TABLE + " (" + ROW_ID +
+            " integer primary key autoincrement, " +
+            CALL_NUMBER_DESC_KEY + " text, " +
+            EVENT_TIME_KEY + " integer);";
+
+
+
+    /**
+     * Listing 8-9: Creating the Content ProviderÕs database
+     */
+    private MySQLiteOpenHelper myOpenHelper;
+
+    @Override
+    public boolean onCreate() {
+        // Construct the underlying database.
+        // Defer opening the database until you need to perform
+        // a query or transaction.
+        myOpenHelper = new MySQLiteOpenHelper(getContext(),
+                DATABASE_NAME, null,
+                DATABASE_VERSION);
+
+        return true;
+    }
+
+
+    /**
+     * Returns the right table name for the given uri
+     * @param uri
+     * @return
+     */
+    private String getTableNameFromUri(Uri uri){
+        switch (uriMatcher.match(uri)) {
+            case ALLCALLS:
+            case SINGLE_CALL:
+                return CALL_TABLE;
+            case ALLEVENTS:
+            case SINGLE_EVENT:
+                return EVENT_TABLE;
+            default: break;
+        }
+
+        return null;
+    }
+
+
+    private Uri getContentUriFromUri(Uri uri){
+        switch (uriMatcher.match(uri)) {
+            case ALLCALLS:
+            case SINGLE_CALL:
+                return CALLS_URI;
+            case ALLEVENTS:
+            case SINGLE_EVENT:
+                return EVENTS_URI;
+            default: break;
+        }
+
+        return null;
+    }
+
+    /**
+     * Listing 8-10: Implementing queries and transactions within a Content Provider
+     */
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+
+        // Open thedatabase.
+        SQLiteDatabase db;
+        try {
+            db = myOpenHelper.getWritableDatabase();
+        } catch (SQLiteException ex) {
+            db = myOpenHelper.getReadableDatabase();
+        }
+
+        // Replace these with valid SQL statements if necessary.
+        String groupBy = null;
+        String having = null;
+
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+
+        // If this is a row query, limit the result set to the passed in row.
+        switch (uriMatcher.match(uri)) {
+
+            case SINGLE_CALL:
+                String callRowID = uri.getPathSegments().get(1);
+                queryBuilder.appendWhere(ROW_ID + "=" + callRowID);
+            break;
+            case SINGLE_EVENT:
+                String eventRowID = uri.getPathSegments().get(1);
+                queryBuilder.appendWhere(ROW_ID + "=" + eventRowID);
+                break;
+            default: break;
+        }
+
+        // Specify the table on which to perform the query. This can
+        // be a specific table or a join as required.
+        queryBuilder.setTables(getTableNameFromUri(uri));
+
+        // Execute the query.
+        Cursor cursor = queryBuilder.query(db, projection, selection,
+                selectionArgs, groupBy, having, sortOrder);
+
+        // Return the result Cursor.
+        return cursor;
+    }
+
+    /**
+     * Listing 8-11: Returning a Content Provider MIME type
+     */
+    @Override
+    public String getType(Uri uri) {
+        // Return a string that identifies the MIME type
+        // for a Content Provider URI
+        switch (uriMatcher.match(uri)) {
+            case ALLEVENTS:
+                return "vnd.android.cursor.dir/vnd.fede.event";
+            case SINGLE_EVENT:
+                return "vnd.android.cursor.item/vnd.fede.event";
+            case ALLCALLS:
+                return "vnd.android.cursor.dir/vnd.fede.call";
+            case SINGLE_CALL:
+                return "vnd.android.cursor.item/vnd.fede.call";
+            default:
+                throw new IllegalArgumentException("Unsupported URI: " +
+                        uri);
+        }
+    }
+
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        SQLiteDatabase db = myOpenHelper.getWritableDatabase();
+
+        switch (uriMatcher.match(uri)) {
+            case SINGLE_CALL :
+            case SINGLE_EVENT:
+                String rowID = uri.getPathSegments().get(1);
+                selection = ROW_ID + "=" + rowID
+                        + (!TextUtils.isEmpty(selection) ?
+                        " AND (" + selection + ')' : "");
+            default: break;
+        }
+
+
+        if (selection == null)
+            selection = "1";
+
+        int deleteCount = db.delete(getTableNameFromUri(uri),
+                selection, selectionArgs);
+
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        return deleteCount;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        SQLiteDatabase db = myOpenHelper.getWritableDatabase();
+
+
+        String nullColumnHack = null;
+
+        long id = db.insert(getTableNameFromUri(uri),
+                nullColumnHack, values);
+
+        if (id > -1) {
+            Uri insertedId = ContentUris.withAppendedId(getContentUriFromUri(uri), id);
+            getContext().getContentResolver().notifyChange(insertedId, null);
+
+            return insertedId;
+        }
+        else
+            return null;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+
+        // Open a read / write database to support the transaction.
+        SQLiteDatabase db = myOpenHelper.getWritableDatabase();
+
+        // If this is a row URI, limit the deletion to the specified row.
+        switch (uriMatcher.match(uri)) {
+            case SINGLE_CALL :
+            case SINGLE_EVENT:
+                String rowID = uri.getPathSegments().get(1);
+                selection = ROW_ID + "=" + rowID
+                        + (!TextUtils.isEmpty(selection) ?
+                        " AND (" + selection + ')' : "");
+            default: break;
+        }
+
+        // Perform the update.
+        int updateCount = db.update(getTableNameFromUri(uri),
+                values, selection, selectionArgs);
+
+        // Notify any observers of the change in the data set.
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        return updateCount;
+    }
+
+    private static class MySQLiteOpenHelper extends SQLiteOpenHelper {
+
+
+
+
+
+        public MySQLiteOpenHelper(Context context, String name,
+                                  CursorFactory factory, int version) {
+            super(context, name, factory, version);
+        }
+
+        // Called when no database exists in disk and the helper class needs
+        // to create a new one.
+        @Override
+        public void onCreate(SQLiteDatabase _db) {
+            _db.execSQL(DATABASE_EVENT_CREATE);
+            _db.execSQL(DATABASE_CALLS_CREATE);
+        }
+
+        // Called when there is a database version mismatch meaning that the version
+        // of the database on disk needs to be upgraded to the current version.
+        @Override
+        public void onUpgrade(SQLiteDatabase _db, int _oldVersion, int _newVersion) {
+            // Log the version upgrade.
+            Log.w("TaskDBAdapter", "Upgrading from version " +
+                    _oldVersion + " to " +
+                    _newVersion + ", which will destroy all old data");
+
+            // Upgrade the existing database to conform to the new version. Multiple
+            // previous versions can be handled by comparing _oldVersion and _newVersion
+            // values.
+
+            // The simplest case is to drop the old table and create a new one.
+            _db.execSQL("DROP TABLE IF IT EXISTS " + CALL_TABLE);
+            _db.execSQL("DROP TABLE IF IT EXISTS " + EVENT_TABLE);
+            // Create a new one.
+            onCreate(_db);
+        }
+    }
+}
